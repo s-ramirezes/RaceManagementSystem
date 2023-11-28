f7ab78f36ceff1f071520e8faa23b0d322cc6c34
s-ramirezes <s_ramirezes@uncg.edu>
2023-11-27 19:46:37 -0500

    added access denied page

--- RMSproj/src/main/java/com/CSC340/RMSproj/LoginController.java
+++ RMSproj/src/main/java/com/CSC340/RMSproj/LoginController.java
@@ -17,4 +17,9 @@ public class LoginController {
     public String login() {
         return "login";
     }
+
+    @GetMapping("/403")
+    public String accessDenied(){
+        return "403";
+    }
 }
